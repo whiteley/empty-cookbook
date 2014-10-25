059b1d3710e303ebf7e3934e66e288ea67c25ba9
Matt Whiteley <mwhiteley@engineyard.com>
2014-10-24 20:34:26 -0700

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'empty-cookbook'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures empty-cookbook'
+long_description 'Installs/Configures empty-cookbook'
+version          '0.1.0'
+
